cf3f7f584db56a35e9489f3e367697efa4be7c83
Heytemisan <temyicy@gmail.com>
2022-05-20 22:48:37 +0100

    feat: created a function to remove an existing directory using the ~rmdir method

--- 02tut/dir.js
+++ 02tut/dir.js
@@ -6,3 +6,10 @@ if (!fs.existsSync('./new')) {
         return console.log('New directory created');
     })
 }
+
+if (fs.existsSync('./new')) {
+    fs.rmdir('./new', function (err) {
+        if (err) throw err;
+        return console.log('Directory removed');
+    })
+} 
