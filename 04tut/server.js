3dca29b271d4ace6ef4f8a04345ebf5668ae68c6
Heytemisan <temyicy@gmail.com>
2022-05-22 15:51:33 +0100

    feat: server 404 response

--- 04tut/server.js
+++ 04tut/server.js
@@ -1,10 +1,11 @@
 const http = require('http');
 const path = require('path');
-const fs = require('fs');
-const fsPromises = require('fs').promises; //common required modules
-
-const logEvents = require('./logEvents');
+const fs = require('fs')
+// const logEvents from './logEvents';
 const EventEmitter = require('events');
+//common required modules
+
+const fsPromises = require('fs').promises;
 
 class Emitter extends EventEmitter { };
 
@@ -13,10 +14,86 @@ const myEmitter = new Emitter();
 
 const PORT = process.env.PORT || 3500;
 
+const serveFile = async (filePath, contentType, response)=>{
+    try {
+        const data = await fsPromises.readFile(filePath, 'utf8');
+        response.writeHead(200, { 'Content-Type': contentType });
+        response.end(data);
+    } catch (error) {
+        console.log(error);
+        response.statusCode = 500;
+        response.end();
+    }
+}
+
+//server listening address
 const server = http.createServer((req, res) => {
+
     console.log(req.url, req.method);
 
-    
-})
+    //build a path and then serve the file 
+    const extension = path.extname(req.url);
+
+    let contentType;
+
+    switch (extension) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case 'png':
+            contentType = 'image/png';
+            break;
+        case '.txt':
+            contentType = 'text/plain'
+            break
+        default:
+            contentType = 'text/html';
+    }
+
+    //chain tenairy statement
+
+    let filePath =
+        contentType == 'text/html' && req.url === '/'
+            ? path.join(__dirname, 'views', 'index.html')
+            : contentType == 'text/html' && req.url.slice(-1) === '1'
+                ? path.join(__dirname, 'views', req.url, 'index.html')
+                : contentType == 'text/html'
+                    ? path.join(__dirname, 'views', req.url)
+                    : path.join(__dirname, req.url);
+
+    //makes .html extension not required in the browser
+    if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
+
+    const fileExists = fs.existsSync(filePath);
+
+    //condition to check if fileExists 
+    if (fileExists) {
+        serveFile(filePath, contentType, res);
+    } else {
+        switch (path.parse(filePath).base) {
+            case 'old-page-html':
+                res.writeHead(301, { 'Location': '/new-page-html' });
+                res.end();
+                break;
+            case 'www-page-html':
+                res.writeHead(301, { 'Location': '/' });
+                res.end();
+                break;
+            default: 
+                //serve a 404 response
+                serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
+        }
+    }
+});
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
