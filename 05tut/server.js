c3ad07a744c3cbf9923a6b21b0ed0ae329e1517e
Heytemisan <temyicy@gmail.com>
2022-05-24 16:43:29 +0100

    feat: cors domains allowed to the backend

--- 05tut/server.js
+++ 05tut/server.js
@@ -12,6 +12,18 @@ const errorHandler = require('./middleware/errorHandler');
 app.use(logger);
 
 //cors ~ cross origin resource sharing
+//domains allowed to access our backend
+const whitelist = ['https://www.site.com', 'http://127.0.0.1:5000', 'http://localhost:3500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by Cors'))
+        }
+    },
+    optionsSucessStatus: 200
+}
 
 app.use(cors())
 //built in middleware to handle urlencoded data
